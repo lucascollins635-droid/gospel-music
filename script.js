52e8ec7ec2031b94e40b22370e5719e89e913ab7
lucascollins635-droid <lucascollins635@gmail.com>
2026-02-12 18:30:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,37 @@
 let songs = [
-  "song1.mp3",
-  "song2.mp3",
-  "song3.mp3",
-  "song4.mp3"
+    "song1.mp3",
+    "song2.mp3",
+    "song3.mp3",
+    "song4.mp3"
 ];
 
 let currentSong = 0;
 let audio = new Audio();
-let startTime;
 let mixDuration = 60 * 60 * 1000; // 1 hour
-let fadeInterval;
-let timerInterval;
+let mixTimeout;
 
 function playMusic() {
-  startTime = Date.now();
-  currentSong = 0;
-  playNext();
-  startTimer();
+    currentSong = 0;
+    playSong(currentSong);
+
+    mixTimeout = setTimeout(() => {
+        audio.pause();
+        alert("🔥 1 Hour Worship Mix Finished!");
+    }, mixDuration);
 }
 
-function playNext() {
+function playSong(index) {
+    audio.src = songs[index];
+    audio.play();
 
-  if (Date.now() - startTime >= mixDuration) {
-    audio.pause();
-    document.getElementById("nowPlaying").innerText = "🔥 Worship Completed!";
-    return;
-  }
+    document.getElementById("nowPlaying").innerText =
+        "Now Playing: " + songs[index];
 
-  audio.src = songs[currentSong];
-  audio.volume = 0;
-  audio.play();
-
-  document.getElementById("nowPlaying").innerText =
-    "Now Playing: " + songs[currentSong];
-
-  fadeIn();
-
-  audio.onended = function () {
-    currentSong++;
-    if (currentSong >= songs.length) {
-      currentSong = 0;
-    }
-    playNext();
-  };
-}
-
-function fadeIn() {
-  let volume = 0;
-  fadeInterval = setInterval(function () {
-    if (volume < 1) {
-      volume += 0.05;
-      audio.volume = volume;
-    } else {
-      clearInterval(fadeInterval);
-    }
-  }, 200);
-}
-
-function startTimer() {
-  timerInterval = setInterval(function () {
-    let timeLeft = mixDuration - (Date.now() - startTime);
-
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
-      return;
-    }
-
-    let minutes = Math.floor(timeLeft / 60000);
-    let seconds = Math.floor((timeLeft % 60000) / 1000);
-
-    if (seconds < 10) seconds = "0" + seconds;
-
-    document.getElementById("timer").innerText =
-      "Time Left: " + minutes + ":" + seconds;
-  }, 1000);
+    audio.onended = function () {
+        currentSong++;
+        if (currentSong >= songs.length) {
+            currentSong = 0;
+        }
+        playSong(currentSong);
+    };
 }
