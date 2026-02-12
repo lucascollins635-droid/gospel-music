7e04b4a4bfbecb2fefa576eae75859b602a7703b
lucascollins635-droid <lucascollins635@gmail.com>
2026-02-12 16:58:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
-let songs = [
-    "assets/song1.mp3",
-    "assets/song2.mp3",
-    "assets/song3.mp3",
-    "assets/song4.mp3"
-];
+const audio = new Audio("assets/audio.mp3");
 
-let currentSong = 0;
-let audio = new Audio();
-audio.volume = 0;
-
-function startMusic() {
-    playSong(currentSong);
-}
-
-function playSong(index) {
-    audio.src = songs[index];
+function playMusic() {
     audio.play();
-
-    fadeIn();
-
-    audio.onended = function() {
-        currentSong++;
-        if (currentSong < songs.length) {
-            playSong(currentSong);
-        } else {
-            alert("Worship session finished 🙏🔥");
-        }
-    };
 }
 
-function fadeIn() {
-    let fade = setInterval(() => {
-        if (audio.volume < 1) {
-            audio.volume += 0.05;
-        } else {
-            clearInterval(fade);
-        }
-    }, 200);
+function pauseMusic() {
+    audio.pause();
+}
+
+function stopMusic() {
+    audio.pause();
+    audio.currentTime = 0;
 }
